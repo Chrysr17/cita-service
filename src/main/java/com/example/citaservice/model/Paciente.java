dc1a91ad00975dc9bb49f5ef68a274d8afa23b99
Chrysr17 <chrisrasaro@gmail.com>
2025-10-29 21:33:16 -0500

    Paciente

--- src/main/java/com/example/citaservice/model/Paciente.java
+++ src/main/java/com/example/citaservice/model/Paciente.java
@@ -1,4 +1,29 @@
 package com.example.citaservice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "paciente")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_paciente")
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombres;
+
+    @Column(nullable = false)
+    private String apellidos;
+
+    @Column(length = 8, nullable = false, unique = true)
+    private String dni;
+
 }
