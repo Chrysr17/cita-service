7508ce4793d380021d290e895a203cc6c06d4408
Chrysr17 <chrisrasaro@gmail.com>
2025-10-29 21:28:39 -0500

    Cita

--- src/main/java/com/example/citaservice/model/Cita.java
+++ src/main/java/com/example/citaservice/model/Cita.java
@@ -1,4 +1,34 @@
 package com.example.citaservice.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "cita")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cita {
+
+    private Long id;
+    private Paciente paciente;
+
+    @ManyToOne
+    @JoinColumns({
+            @JoinColumn(name = "id_horario", referencedColumnName = "id_horario"),
+            @JoinColumn(name = "id_medico", referencedColumnName = "id_medico"),
+            @JoinColumn(name = "id_hora", referencedColumnName = "id_hora")
+    })
+    private HorarioMedicoHora horarioMedicoHora;
+
+    @Column(name = "fecha_cita")
+    private LocalDate fechaCita;
+
+    @Enumerated(EnumType.STRING)
+    private EstadoCita estadoCita;
+
 }
