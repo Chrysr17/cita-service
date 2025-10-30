729b0c2d858d4c50815794e9d2c8916b7d3d64f9
Chrysr17 <chrisrasaro@gmail.com>
2025-10-29 21:36:47 -0500

    HorarioMedicoId

--- src/main/java/com/example/citaservice/model/HorarioMedicoHora.java
+++ src/main/java/com/example/citaservice/model/HorarioMedicoHora.java
@@ -2,6 +2,7 @@ package com.example.citaservice.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +14,7 @@ import org.springframework.data.annotation.Id;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@IdClass(HorarioMedicoHoraId.class)
 public class HorarioMedicoHora {
     @Id
     @Column(name = "id_horario")
--- src/main/java/com/example/citaservice/model/HorarioMedicoHoraId.java
+++ src/main/java/com/example/citaservice/model/HorarioMedicoHoraId.java
@@ -1,4 +1,17 @@
 package com.example.citaservice.model;
 
-public class HorarioMedicoHoraId {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HorarioMedicoHoraId implements Serializable {
+    private Long idHorario;
+    private Long idMedico;
+    private Long idHora;
+
 }
