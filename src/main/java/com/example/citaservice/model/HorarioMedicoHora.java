cba0cbe717b3a842782d37e6e51876e9b6f49130
Chrysr17 <chrisrasaro@gmail.com>
2025-10-29 21:35:39 -0500

    HorarioMedico

--- src/main/java/com/example/citaservice/model/HorarioMedicoHora.java
+++ src/main/java/com/example/citaservice/model/HorarioMedicoHora.java
@@ -1,4 +1,34 @@
 package com.example.citaservice.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "horario_medico_hora")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HorarioMedicoHora {
+    @Id
+    @Column(name = "id_horario")
+    private Long idHorario;
+
+    @Id
+    @Column(name = "id_medico")
+    private Long idMedico;
+
+    @Id
+    @Column(name = "id_hora")
+    private Long idHora;
+
+    @Column(name = "id_consultorio")
+    private Long idConsultorio;
+
+    @Column(name = "estado", length = 10)
+    private String estado; // Disponible / Ocupado
 }
