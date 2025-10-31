7bec760a896d5fa9a57d5b5af83ae1428e257bdd
Chrysr17 <chrisrasaro@gmail.com>
2025-10-31 17:57:40 -0500

    CitaService

--- src/main/java/com/example/citaservice/service/CitaService.java
+++ src/main/java/com/example/citaservice/service/CitaService.java
@@ -1,4 +1,12 @@
 package com.example.citaservice.service;
 
+import com.example.citaservice.dto.CitaRequest;
+import com.example.citaservice.model.Cita;
+
+import java.util.List;
+
 public interface CitaService {
+    Cita registrarCita(CitaRequest request);
+    List<Cita> listarCitasPorPaciente(Long idPaciente);
+    void cancelarCita(Long idCita);
 }
--- src/main/java/com/example/citaservice/service/impl/CitaServiceImpl.java
+++ src/main/java/com/example/citaservice/service/impl/CitaServiceImpl.java
@@ -1,4 +1,24 @@
 package com.example.citaservice.service.impl;
 
-public class CitaServiceImpl {
+import com.example.citaservice.dto.CitaRequest;
+import com.example.citaservice.model.Cita;
+import com.example.citaservice.service.CitaService;
+
+import java.util.List;
+
+public class CitaServiceImpl implements CitaService {
+    @Override
+    public Cita registrarCita(CitaRequest request) {
+        return null;
+    }
+
+    @Override
+    public List<Cita> listarCitasPorPaciente(Long idPaciente) {
+        return List.of();
+    }
+
+    @Override
+    public void cancelarCita(Long idCita) {
+
+    }
 }
